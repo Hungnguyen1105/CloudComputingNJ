042a23b5d4cef4a2df3d2750380ff8afda907d7b
Hungnguyen1105 <hbday2k3@gmail.com>
2023-09-29 17:21:16 +0700

    weblab05

--- lab05/web02/app.js
+++ lab05/web02/app.js
@@ -6,19 +6,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//khai báo router mới (tạo file "mobile.js" trong thư mục "routes")
+var mobileRouter = require('./routes/mobile');
 
 var app = express();
 
-// khai bao va cau hinh thuw vien mongoose de lam vc voi DB
-const mongoose = require('mongoose');
-var uri = 'mongodb://localhost:27017/';//cloud local
-var uri1 = 'mongodb+srv://hbday2k3:hungnguyen2003@cluster0.f3h6kv6.mongodb.net/';//cloud db
-mongoose.connect(uri1)
-.then(()=> console.log('Connect to DB succeed !'))
-.catch((err) => console.log(err));
+//khai báo & cấu hình thư viện body-parser để lấy dữ liệu từ form
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//cấu hình url cho router mới
+app.use('/mobile', mobileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +45,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//cấu hình port cho server để deploy lên Render
+app.listen(process.env.PORT || 3001);
 
 module.exports = app;
